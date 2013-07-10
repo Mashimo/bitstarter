d2b328ea11d8280aa835ccc573b4ed40c4aa8936
Mashimo <mashimo9000@yahoo.de>
2013-07-10 19:30:44 +0000

    reading string from html file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = new Buffer(256);
+  var str;
+  var content = fs.readFileSync("index.html", function (err, data) {
+      if (err) throw err;
+      str = buf.toString (); 
+  });
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
